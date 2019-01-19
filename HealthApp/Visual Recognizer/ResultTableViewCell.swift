7916c532fdeb10c9944cdab1fb465f2755393a92
Moisés Córdova <kernel_linux@outlook.com>
2019-01-19 14:32:22 -0600

    Added first version of trained analyzer system CameraViewController: Takes a screenshot of camera PorgressBar: Tint bar depending how accurate is the prediction Other Files: Works directly in card view table

---
+++ HealthApp/Visual Recognizer/ResultTableViewCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var progress: ProgressBar!
+
+}
