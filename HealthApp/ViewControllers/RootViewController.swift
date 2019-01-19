9fbe884a06d3ffa7f0f7f6d1cccacd12cc28fb6f
Moisés Córdova <kernel_linux@outlook.com>
2019-01-19 14:30:19 -0600

    RootViewController for slide view added SinglePageViewController added: 	Displays an array of some patient data

---
+++ HealthApp/ViewControllers/RootViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RootViewController.swift
+//  HealthApp
+//
+//  Created by Moisés Córdova on 1/16/19.
+//  Copyright © 2019 Moisés Córdova. All rights reserved.
+//
+
+import UIKit
+
+class SinglePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
