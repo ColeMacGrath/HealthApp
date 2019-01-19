3820b823357558d98c4eaf6705ae83c6d7708527
Moisés Córdova <kernel_linux@outlook.com>
2019-01-19 14:13:08 -0600

    Util File created Some Extensions created 	String: 		* Check by regex if is password 		* Check by regex if is name 	Date: 		* Some vars to get: Yesterday, tomorrow, dayBefore, dayAfter, noon, month of a date 		* startOfDay: Returns first hour of date 		* endOfDay: Return last hour of date 		* offsetFrom: returns in string different in time from two dates 	UIImage: Set rounded 	UITableViewCell: An extensions for textView

---
+++ HealthApp/Support Files/Util.swift
@@ -0,0 +1,123 @@
+import Foundation
+import UIKit
+
+extension String {
+    var isPassword: Bool {
+        return !isEmpty && range(of: "[^a-z,A-z,0-9, ]",
+                                 options: .regularExpression) == nil
+    }
+    var isEmail: Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", options: .caseInsensitive)
+            return regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count)) != nil
+        } catch {
+            return false
+        }
+    }
+    
+    var isName: Bool {
+        return !isEmpty && range(of: "[^a-zA-Z,á,Á,É,Í,Ó,Ú,À,È,Ì,Ò,Ù,é,í,ó,ú,à,è,ì,ò,ù,ö,Ö]", options: .regularExpression) == nil
+    }
+}
+
+func createAlert(withMessage: String) -> UIAlertController {
+    let alert = UIAlertController(title: "Error found", message: withMessage, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+    return alert
+}
+
+extension Date {
+    
+    static var yesterday: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: Date().noon)!
+    }
+    static var tomorrow: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: Date().noon)!
+    }
+    var dayBefore: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: noon)!
+    }
+    var dayAfter: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: noon)!
+    }
+    var noon: Date {
+        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
+    }
+    var month: Int {
+        return Calendar.current.component(.month,  from: self)
+    }
+    
+    var isLastDayOfMonth: Bool {
+        return dayAfter.month != month
+    }
+    
+    var startOfDay: Date {
+        let calendar = Calendar.current
+        let unitFlags = Set<Calendar.Component>([.year, .month, .day])
+        let components = calendar.dateComponents(unitFlags, from: self)
+        return calendar.date(from: components)!
+    }
+    
+    var endOfDay: Date {
+        var components = DateComponents()
+        components.day = 1
+        let date = Calendar.current.date(byAdding: components, to: self.startOfDay)
+        return (date?.addingTimeInterval(-1))!
+    }
+    
+    var formattedDate: String {
+        let date: Date = self
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM dd, yyyy"
+        let formattedDate = dateFormatter.string(from: date)
+        return formattedDate
+    }
+    
+    var formattedHour: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm a"
+        let hourString = formatter.string(from: self)
+        return hourString
+    }
+    
+    func offsetFrom(date : Date) -> String {
+        
+        let dayHourMinuteSecond: Set<Calendar.Component> = [.day, .hour, .minute, .second]
+        let difference = NSCalendar.current.dateComponents(dayHourMinuteSecond, from: date, to: self);
+        
+        let seconds = "\(difference.second ?? 0)s"
+        let minutes = "\(difference.minute ?? 0)m" + " " + seconds
+        let hours = "\(difference.hour ?? 0)h" + " " + minutes
+        let days = "\(difference.day ?? 0)d" + " " + hours
+        
+        if let day = difference.day, day          > 0 { return days }
+        if let hour = difference.hour, hour       > 0 { return hours }
+        if let minute = difference.minute, minute > 0 { return minutes }
+        if let second = difference.second, second > 0 { return seconds }
+        return ""
+    }
+}
+
+extension UIImageView {
+    func setRounded() {
+        self.layer.borderWidth = 0
+        self.layer.masksToBounds = false
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.cornerRadius = self.frame.height/2
+        self.clipsToBounds = true
+    }
+}
+
+extension UITableViewCell {
+    /// Search up the view hierarchy of the table view cell to find the containing table view
+    var tableView: UITableView? {
+        get {
+            var table: UIView? = superview
+            while !(table is UITableView) && table != nil {
+                table = table?.superview
+            }
+            
+            return table as? UITableView
+        }
+    }
+}
