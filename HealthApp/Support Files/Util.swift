0b2bb8662c00d4a577f2397e50d4b5ef347e7ddd
Moisés Córdova <kernel_linux@outlook.com>
2019-07-05 18:36:33 -0500

    New icon for health records Last commit before redesign

--- HealthApp/Support Files/Util.swift
+++ HealthApp/Support Files/Util.swift
@@ -121,3 +121,34 @@ extension UITableViewCell {
         }
     }
 }
+
+@IBDesignable class RoundButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+    
+    func sharedInit() {
+        refreshCorners(value: cornerRadius)
+    }
+    
+    func refreshCorners(value: CGFloat) {
+        layer.cornerRadius = value
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 15 {
+        didSet {
+            refreshCorners(value: cornerRadius)
+        }
+    }
+}
---
+++ HealthApp/Testing Views/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  HealthApp
+//
+//  Created by Moisés Córdova on 7/5/19.
+//  Copyright © 2019 Moisés Córdova. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
