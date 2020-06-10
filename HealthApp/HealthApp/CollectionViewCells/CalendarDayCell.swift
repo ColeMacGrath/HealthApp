f888ee989194746af9e4f3131946918de804b853
Moisés Córdova <kernel_linux@outlook.com>
2020-06-10 12:34:34 -0500

    Added some calendar fixes and Podfile

---
+++ HealthApp/HealthApp/CollectionViewCells/CalendarDayCell.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarDayCell.swift
+//  TestingCalendar
+//
+//  Created by Moisés Córdova on 6/17/19.
+//  Copyright © 2019 Moisés Córdova. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarDayCell: JTACDayCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIView!
+    
+}
---
+++ HealthApp/HealthApp/ViewControllers/CalendarViewController.swift
@@ -0,0 +1,174 @@
+//
+//  CalendarViewController.swift
+//  HealthApp
+//
+//  Created by Moisés Córdova on 10/06/20.
+//  Copyright © 2020 Moisés Córdova. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarViewController: UIViewController {
+    
+    @IBOutlet weak var calendarView: JTACMonthView!
+    @IBOutlet weak var tableView: UITableView!
+    let formatter = DateFormatter()
+    //let outsideMonthColor = UIColor.lightGray
+    let monthColor = UIColor.darkGray
+    let selectedMonthColor = UIColor.white
+    let currentDateSelectedViewColor = UIColor.black
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.calendarView.selectDates([Date()])
+        self.calendarView.scrollToDate(Date(), animateScroll: true)
+        
+        self.calendarView.minimumLineSpacing = 0
+        self.calendarView.minimumInteritemSpacing = 0
+    }
+
+}
+
+extension CalendarViewController: JTACMonthViewDataSource {
+    
+    func configureCalendar(_ calendar: JTACMonthView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        var parameters: ConfigurationParameters
+        var startDate = Date()
+        var endDate = Date()
+        if let calendarStartDate = formatter.date(from: "2019 01 01"),
+            let calendarEndndDate = formatter.date(from: "2019 12 31") {
+            startDate = calendarStartDate
+            endDate = calendarEndndDate
+        }
+        startDate = Date()
+        endDate = Date()
+        parameters = ConfigurationParameters(startDate: startDate, endDate: endDate, numberOfRows: 1)
+        return parameters
+    }
+}
+
+extension CalendarViewController: JTACMonthViewDelegate {
+    func calendar(_ calendar: JTACMonthView, willDisplay cell: JTACDayCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalendarDayCell", for: indexPath) as! CalendarDayCell
+        cell.dateLabel.text = cellState.text
+        
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTACMonthView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTACDayCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalendarDayCell", for: indexPath) as! CalendarDayCell
+        cell.dateLabel.text = cellState.text
+        
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+        
+        return cell
+    }
+    
+    func calendar(_ calendar: JTACMonthView, didSelectDate date: Date, cell: JTACDayCell?, cellState: CellState, indexPath: IndexPath) {
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+        
+        
+        //Show data with date
+    }
+    
+    func calendar(_ calendar: JTACMonthView, didDeselectDate date: Date, cell: JTACDayCell?, cellState: CellState, indexPath: IndexPath) {
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTACMonthView, didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
+        setupViewsFromCalendar(from: visibleDates)
+    }
+}
+
+extension CalendarViewController {
+    func handleCellSelected(view: JTACDayCell?, cellState: CellState) {
+        guard let validCell = view as? CalendarDayCell else { return }
+        if cellState.isSelected {
+            validCell.selectedView.isHidden = false
+        } else {
+            validCell.selectedView.isHidden = true
+        }
+    }
+    
+    func handleCellTextColor(view: JTACDayCell?, cellState: CellState) {
+        guard let validCell = view as? CalendarDayCell else {
+            return
+        }
+        
+        let todaysDate = Date()
+        /*if todaysDate.day == cellState.date.day {
+            validCell.dateLabel.textColor = UIColor.purple
+        } else {
+            validCell.dateLabel.textColor = cellState.isSelected ? UIColor.purple : UIColor.darkGray
+        }*/
+        
+        
+        /*if cellState.isSelected {
+            //validCell.dateLabel.textColor = selectedMonthColor
+        } else {
+            if cellState.dateBelongsTo == .thisMonth {
+                //validCell.dateLabel.textColor = monthColor
+            } else {
+                //validCell.dateLabel.textColor = outsideMonthColor
+            }
+        }*/
+    }
+    
+    func setupCalendarView() {
+        // Setup Calendar Spacing
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
+    }
+    
+    func setupViewsFromCalendar(from visibleDates: DateSegmentInfo ) {
+        guard let date = visibleDates.monthDates.first?.date else { return }
+        
+        formatter.dateFormat = "MMMM"
+        title = formatter.string(from: date).uppercased()
+    }
+    
+}
+
+extension CalendarViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AppointmentCell", for: indexPath) as! AppointmentTableViewCell
+        cell.appointmentTitleLabel.text = "Appointment"
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //TO DO
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action =  UIContextualAction(style: .destructive, title: "Remove", handler: { (_,_,_ ) in
+        })
+        action.image = UIImage(named: "trash-circle")
+        action.backgroundColor = .lightGray
+        let swipeActionsConfiguration = UISwipeActionsConfiguration(actions: [action])
+        
+        return swipeActionsConfiguration
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130.0
+    }
+}
